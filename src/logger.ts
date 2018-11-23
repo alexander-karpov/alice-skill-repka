bc0d35189261e8465175f54d6c6c001b21dd1e1d
Alexander Karpov <kukuruku@yandex-team.ru>
2018-11-23 23:53:31 +0300

    Меняет название лог-файла чтобы проще читать его с nginx.

--- src/logger.ts
+++ src/logger.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 export function openLogFile(logsDir: string): number {
-    return fs.openSync(path.join(logsDir, `./request.log`), 'a');
+    return fs.openSync(path.join(logsDir, `./log.txt`), 'a');
 }
 
 export function appendToLog(fd: number, record: string) {
