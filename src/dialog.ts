eec5653e0b9e67dbecec2d0cb47d453c7541ee1c
Alexander Karpov <kukuruku@yandex-team.ru>
2018-12-24 03:10:37 +0300

    Увеличивает раунд обычной игры до 12, черной уменьшает до 8.

--- src/dialog.ts
+++ src/dialog.ts
@@ -35,7 +35,7 @@ export async function mainDialog(
     { stemmer, random100 }: DialogDependencies,
 ): Promise<DialogResult> {
     const isRepeatQuestionDialog = sessionData.currentDialog === Dialogs.RepeatQuestion;
-
+    const mode = sessionData.mode;
     const tokens = await stemmer(rawTokens.join(' '));
     const { chars } = sessionData;
 
@@ -85,7 +85,7 @@ export async function mainDialog(
         if (!nextChar && inanimate) {
             return {
                 speech: answers.inanimateCalled(inanimate, currentChar),
-                buttons: makeButtons(chars, random100),
+                buttons: makeButtons(chars, mode, random100),
                 endSession: false,
             };
         }
@@ -93,7 +93,7 @@ export async function mainDialog(
         if (!nextChar) {
             return {
                 speech: answers.wrongCommand(sessionData),
-                buttons: makeButtons(chars, random100),
+                buttons: makeButtons(chars, mode, random100),
                 endSession: false,
             };
         }
@@ -103,9 +103,9 @@ export async function mainDialog(
         }
 
         chars.push(nextChar);
-        const tale = makeRepkaStory(chars, nextChar);
+        const tale = makeRepkaStory(chars, nextChar, mode);
 
-        if (isEndOfStory(chars)) {
+        if (isEndOfStory(chars, mode)) {
             sessionData.currentDialog = Dialogs.RepeatQuestion;
         }
 
@@ -121,7 +121,7 @@ export async function mainDialog(
                 return {
                     speech: speak(knownCharTts, tale),
                     imageId: knownChar.image,
-                    buttons: isEndOfStory(chars) ? storyEndButtons() : undefined,
+                    buttons: isEndOfStory(chars, mode) ? storyEndButtons() : undefined,
                     endSession: false,
                 };
             }
@@ -129,14 +129,14 @@ export async function mainDialog(
             return {
                 speech: speak(knownCharAnswer, tale),
                 imageId: '',
-                buttons: makeButtons(chars, random100),
+                buttons: makeButtons(chars, mode, random100),
                 endSession: false,
             };
         }
 
         return {
             speech: tale,
-            buttons: makeButtons(chars, random100),
+            buttons: makeButtons(chars, mode, random100),
             endSession: false,
         };
     })();
@@ -144,14 +144,14 @@ export async function mainDialog(
     return isDialogResult(result) ? result : { speech: result, endSession: false };
 }
 
-function makeRepkaStory(all: Character[], char: Character) {
+function makeRepkaStory(all: Character[], char: Character, mode: GameMode) {
     return speak(
         answers.formatStory(all),
-        isEndOfStory(all) ? answers.success() : answers.failure(char),
+        isEndOfStory(all, mode) ? answers.success() : answers.failure(char),
     );
 }
 
-function isEndOfStory(chars: Character[]) {
+function isEndOfStory(chars: Character[], mode: GameMode) {
     const last = _.last(chars);
 
     if (!last) {
@@ -159,7 +159,8 @@ function isEndOfStory(chars: Character[]) {
     }
 
     const isLastMouse = last.normal.startsWith('мыш');
-    const tooManyCharacters = chars.length >= 10;
+    const maxCharsCount = mode === GameMode.Classic ? 12 : 8;
+    const tooManyCharacters = chars.length >= maxCharsCount;
     return isLastMouse || tooManyCharacters;
 }
 
@@ -167,14 +168,14 @@ function isDialogResult(x): x is DialogResult {
     return typeof x.speech === 'object' && typeof x.endSession === 'boolean';
 }
 
-function makeButtons(chars: Character[], random100: number): DialogButton[] {
+function makeButtons(chars: Character[], mode: GameMode, random100: number): DialogButton[] {
     // Дадим ребенку сначала понять, что можно
     // самому придумывать персонажей.
     if (chars.length < 3) {
         return [];
     }
 
-    if (isEndOfStory(chars)) {
+    if (isEndOfStory(chars, mode)) {
         return storyEndButtons();
     }
 
