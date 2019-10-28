dace647db5da7c28896d91444ee2d06c5ad78e60
Alexander Karpov <kukuruku@yandex-team.ru>
2019-10-28 07:42:24 +0300

    Обновил wordstat.txt

--- tools/wordstat.js
+++ tools/wordstat.js
@@ -1,18 +1,20 @@
-const axios = require('axios');
+const fs = require('fs');
 const _ = require('lodash');
+const readline = require('readline');
 const { logsUrl } = require('./secret.json');
 const { spawnMystem } = require('./stemmer');
 
-async function printStats(log) {
+async function printStats(records) {
     const { stemmer, killStemmer } = spawnMystem();
 
-    const records = splitRequests(log.data)
-        .map(extractMessage)
-        .filter(Boolean);
-
     const counters = {};
-
+    let i = 0;
     for (let word of records) {
+        i++;
+        if (i % 100 === 0) {
+            console.log(Math.floor((i / records.length) * 100));
+        }
+
         const lemmas = await stemmer(word);
         const clear = lemmas.replace(/{|}/g, '');
 
@@ -26,16 +28,29 @@ async function printStats(log) {
     killStemmer();
 
     const mapped = Object.keys(counters).map(key => ({ word: key, count: counters[key] }));
-    const sorted = _.sortBy(mapped, o => -o.count);
-    sorted.forEach(r => console.log(`${r.word} ${r.count}`));
+
+    const stat =
+        _.sortBy(mapped.filter(w => w.count >= 10), o => -o.count)
+            .map(r => `${r.word} ${r.count}`)
+            .join('\n') + '\n';
+
+    fs.writeFileSync('./wordstat.txt', stat);
 }
 
-function splitRequests(text) {
-    return text.split('\n\n\n');
-}
+var lineReader = readline.createInterface({
+    input: fs.createReadStream('/Users/kukuruku/Documents/logs/repka/all.txt'),
+});
 
-function extractMessage(req) {
-    return req.split('\n')[1];
-}
+let prevIsDate = false;
+const messages = [];
+lineReader.on('line', function(line) {
+    if (prevIsDate && line) {
+        messages.push(line);
+    }
 
-axios.get(logsUrl).then(printStats);
+    prevIsDate = line.includes('mo ');
+});
+
+lineReader.on('close', function() {
+    printStats(messages);
+});
